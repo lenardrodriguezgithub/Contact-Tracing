79304b54f1139e4b36bfb9de5fdbc1dbde0e2701
Lenard Rodriguez <rodriguezjanlenard.new@gmail.com>
2022-06-06 21:38:17 +0800

    added streamwriter

--- Contact Tracing/Form1.Designer.cs
+++ Contact Tracing/Form1.Designer.cs
@@ -28,12 +28,412 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.lbl_Province = new System.Windows.Forms.Label();
+            this.lbl_Municipality = new System.Windows.Forms.Label();
+            this.lbl_Barangay = new System.Windows.Forms.Label();
+            this.lbl_Street = new System.Windows.Forms.Label();
+            this.lbl_HouseNumber = new System.Windows.Forms.Label();
+            this.txt_Province = new System.Windows.Forms.TextBox();
+            this.txt_Municipality = new System.Windows.Forms.TextBox();
+            this.txt_Barangay = new System.Windows.Forms.TextBox();
+            this.txt_Street = new System.Windows.Forms.TextBox();
+            this.txt_HouseNumber = new System.Windows.Forms.TextBox();
+            this.lbl_Address = new System.Windows.Forms.Label();
+            this.txt_Contact = new System.Windows.Forms.TextBox();
+            this.lbl_Contact = new System.Windows.Forms.Label();
+            this.txt_Email = new System.Windows.Forms.TextBox();
+            this.lbl_Email = new System.Windows.Forms.Label();
+            this.lbl_Birthdate = new System.Windows.Forms.Label();
+            this.lbl_YearsOld = new System.Windows.Forms.Label();
+            this.dtpckr_Birthdate = new System.Windows.Forms.DateTimePicker();
+            this.txt_Age = new System.Windows.Forms.TextBox();
+            this.lbl_Age = new System.Windows.Forms.Label();
+            this.txt_Others = new System.Windows.Forms.TextBox();
+            this.rbttn_Others = new System.Windows.Forms.RadioButton();
+            this.rbttn_Female = new System.Windows.Forms.RadioButton();
+            this.rbttn_Male = new System.Windows.Forms.RadioButton();
+            this.lbl_Gender = new System.Windows.Forms.Label();
+            this.lbl_LastName = new System.Windows.Forms.Label();
+            this.lbl_FirstName = new System.Windows.Forms.Label();
+            this.txt_LastName = new System.Windows.Forms.TextBox();
+            this.txt_FirstName = new System.Windows.Forms.TextBox();
+            this.lbl_Name = new System.Windows.Forms.Label();
+            this.lbl_PersonalDetails = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.lbl_Province);
+            this.panel1.Controls.Add(this.lbl_Municipality);
+            this.panel1.Controls.Add(this.lbl_Barangay);
+            this.panel1.Controls.Add(this.lbl_Street);
+            this.panel1.Controls.Add(this.lbl_HouseNumber);
+            this.panel1.Controls.Add(this.txt_Province);
+            this.panel1.Controls.Add(this.txt_Municipality);
+            this.panel1.Controls.Add(this.txt_Barangay);
+            this.panel1.Controls.Add(this.txt_Street);
+            this.panel1.Controls.Add(this.txt_HouseNumber);
+            this.panel1.Controls.Add(this.lbl_Address);
+            this.panel1.Controls.Add(this.txt_Contact);
+            this.panel1.Controls.Add(this.lbl_Contact);
+            this.panel1.Controls.Add(this.txt_Email);
+            this.panel1.Controls.Add(this.lbl_Email);
+            this.panel1.Controls.Add(this.lbl_Birthdate);
+            this.panel1.Controls.Add(this.lbl_YearsOld);
+            this.panel1.Controls.Add(this.dtpckr_Birthdate);
+            this.panel1.Controls.Add(this.txt_Age);
+            this.panel1.Controls.Add(this.lbl_Age);
+            this.panel1.Controls.Add(this.txt_Others);
+            this.panel1.Controls.Add(this.rbttn_Others);
+            this.panel1.Controls.Add(this.rbttn_Female);
+            this.panel1.Controls.Add(this.rbttn_Male);
+            this.panel1.Controls.Add(this.lbl_Gender);
+            this.panel1.Controls.Add(this.lbl_LastName);
+            this.panel1.Controls.Add(this.lbl_FirstName);
+            this.panel1.Controls.Add(this.txt_LastName);
+            this.panel1.Controls.Add(this.txt_FirstName);
+            this.panel1.Controls.Add(this.lbl_Name);
+            this.panel1.Controls.Add(this.lbl_PersonalDetails);
+            this.panel1.Location = new System.Drawing.Point(10, 10);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(850, 650);
+            this.panel1.TabIndex = 0;
+            // 
+            // lbl_Province
+            // 
+            this.lbl_Province.AutoSize = true;
+            this.lbl_Province.Location = new System.Drawing.Point(680, 610);
+            this.lbl_Province.Name = "lbl_Province";
+            this.lbl_Province.Size = new System.Drawing.Size(69, 25);
+            this.lbl_Province.TabIndex = 30;
+            this.lbl_Province.Text = "label16";
+            // 
+            // lbl_Municipality
+            // 
+            this.lbl_Municipality.AutoSize = true;
+            this.lbl_Municipality.Location = new System.Drawing.Point(530, 610);
+            this.lbl_Municipality.Name = "lbl_Municipality";
+            this.lbl_Municipality.Size = new System.Drawing.Size(69, 25);
+            this.lbl_Municipality.TabIndex = 29;
+            this.lbl_Municipality.Text = "label15";
+            // 
+            // lbl_Barangay
+            // 
+            this.lbl_Barangay.AutoSize = true;
+            this.lbl_Barangay.Location = new System.Drawing.Point(360, 610);
+            this.lbl_Barangay.Name = "lbl_Barangay";
+            this.lbl_Barangay.Size = new System.Drawing.Size(69, 25);
+            this.lbl_Barangay.TabIndex = 28;
+            this.lbl_Barangay.Text = "label14";
+            // 
+            // lbl_Street
+            // 
+            this.lbl_Street.AutoSize = true;
+            this.lbl_Street.Location = new System.Drawing.Point(210, 610);
+            this.lbl_Street.Name = "lbl_Street";
+            this.lbl_Street.Size = new System.Drawing.Size(69, 25);
+            this.lbl_Street.TabIndex = 27;
+            this.lbl_Street.Text = "label13";
+            // 
+            // lbl_HouseNumber
+            // 
+            this.lbl_HouseNumber.AutoSize = true;
+            this.lbl_HouseNumber.Location = new System.Drawing.Point(40, 610);
+            this.lbl_HouseNumber.Name = "lbl_HouseNumber";
+            this.lbl_HouseNumber.Size = new System.Drawing.Size(69, 25);
+            this.lbl_HouseNumber.TabIndex = 26;
+            this.lbl_HouseNumber.Text = "label12";
+            // 
+            // txt_Province
+            // 
+            this.txt_Province.Location = new System.Drawing.Point(680, 570);
+            this.txt_Province.Name = "txt_Province";
+            this.txt_Province.Size = new System.Drawing.Size(150, 31);
+            this.txt_Province.TabIndex = 25;
+            // 
+            // txt_Municipality
+            // 
+            this.txt_Municipality.Location = new System.Drawing.Point(520, 570);
+            this.txt_Municipality.Name = "txt_Municipality";
+            this.txt_Municipality.Size = new System.Drawing.Size(150, 31);
+            this.txt_Municipality.TabIndex = 24;
+            // 
+            // txt_Barangay
+            // 
+            this.txt_Barangay.Location = new System.Drawing.Point(360, 570);
+            this.txt_Barangay.Name = "txt_Barangay";
+            this.txt_Barangay.Size = new System.Drawing.Size(150, 31);
+            this.txt_Barangay.TabIndex = 23;
+            // 
+            // txt_Street
+            // 
+            this.txt_Street.Location = new System.Drawing.Point(200, 570);
+            this.txt_Street.Name = "txt_Street";
+            this.txt_Street.Size = new System.Drawing.Size(150, 31);
+            this.txt_Street.TabIndex = 22;
+            // 
+            // txt_HouseNumber
+            // 
+            this.txt_HouseNumber.Location = new System.Drawing.Point(40, 570);
+            this.txt_HouseNumber.Name = "txt_HouseNumber";
+            this.txt_HouseNumber.Size = new System.Drawing.Size(150, 31);
+            this.txt_HouseNumber.TabIndex = 21;
+            // 
+            // lbl_Address
+            // 
+            this.lbl_Address.AutoSize = true;
+            this.lbl_Address.Location = new System.Drawing.Point(30, 540);
+            this.lbl_Address.Name = "lbl_Address";
+            this.lbl_Address.Size = new System.Drawing.Size(69, 25);
+            this.lbl_Address.TabIndex = 20;
+            this.lbl_Address.Text = "label11";
+            // 
+            // txt_Contact
+            // 
+            this.txt_Contact.Location = new System.Drawing.Point(40, 490);
+            this.txt_Contact.Name = "txt_Contact";
+            this.txt_Contact.Size = new System.Drawing.Size(320, 31);
+            this.txt_Contact.TabIndex = 19;
+            // 
+            // lbl_Contact
+            // 
+            this.lbl_Contact.AutoSize = true;
+            this.lbl_Contact.Location = new System.Drawing.Point(30, 460);
+            this.lbl_Contact.Name = "lbl_Contact";
+            this.lbl_Contact.Size = new System.Drawing.Size(69, 25);
+            this.lbl_Contact.TabIndex = 18;
+            this.lbl_Contact.Text = "label10";
+            // 
+            // txt_Email
+            // 
+            this.txt_Email.Location = new System.Drawing.Point(40, 410);
+            this.txt_Email.Name = "txt_Email";
+            this.txt_Email.Size = new System.Drawing.Size(320, 31);
+            this.txt_Email.TabIndex = 17;
+            // 
+            // lbl_Email
+            // 
+            this.lbl_Email.AutoSize = true;
+            this.lbl_Email.Location = new System.Drawing.Point(30, 380);
+            this.lbl_Email.Name = "lbl_Email";
+            this.lbl_Email.Size = new System.Drawing.Size(59, 25);
+            this.lbl_Email.TabIndex = 16;
+            this.lbl_Email.Text = "label9";
+            // 
+            // lbl_Birthdate
+            // 
+            this.lbl_Birthdate.AutoSize = true;
+            this.lbl_Birthdate.Location = new System.Drawing.Point(210, 340);
+            this.lbl_Birthdate.Name = "lbl_Birthdate";
+            this.lbl_Birthdate.Size = new System.Drawing.Size(59, 25);
+            this.lbl_Birthdate.TabIndex = 15;
+            this.lbl_Birthdate.Text = "label8";
+            // 
+            // lbl_YearsOld
+            // 
+            this.lbl_YearsOld.AutoSize = true;
+            this.lbl_YearsOld.Location = new System.Drawing.Point(40, 340);
+            this.lbl_YearsOld.Name = "lbl_YearsOld";
+            this.lbl_YearsOld.Size = new System.Drawing.Size(59, 25);
+            this.lbl_YearsOld.TabIndex = 14;
+            this.lbl_YearsOld.Text = "label7";
+            // 
+            // dtpckr_Birthdate
+            // 
+            this.dtpckr_Birthdate.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dtpckr_Birthdate.Location = new System.Drawing.Point(210, 300);
+            this.dtpckr_Birthdate.Name = "dtpckr_Birthdate";
+            this.dtpckr_Birthdate.Size = new System.Drawing.Size(300, 31);
+            this.dtpckr_Birthdate.TabIndex = 13;
+            // 
+            // txt_Age
+            // 
+            this.txt_Age.Location = new System.Drawing.Point(40, 300);
+            this.txt_Age.Name = "txt_Age";
+            this.txt_Age.Size = new System.Drawing.Size(150, 31);
+            this.txt_Age.TabIndex = 12;
+            // 
+            // lbl_Age
+            // 
+            this.lbl_Age.AutoSize = true;
+            this.lbl_Age.Location = new System.Drawing.Point(30, 270);
+            this.lbl_Age.Name = "lbl_Age";
+            this.lbl_Age.Size = new System.Drawing.Size(59, 25);
+            this.lbl_Age.TabIndex = 11;
+            this.lbl_Age.Text = "label6";
+            // 
+            // txt_Others
+            // 
+            this.txt_Others.Location = new System.Drawing.Point(210, 240);
+            this.txt_Others.Name = "txt_Others";
+            this.txt_Others.Size = new System.Drawing.Size(150, 31);
+            this.txt_Others.TabIndex = 10;
+            // 
+            // rbttn_Others
+            // 
+            this.rbttn_Others.AutoSize = true;
+            this.rbttn_Others.Location = new System.Drawing.Point(50, 240);
+            this.rbttn_Others.Name = "rbttn_Others";
+            this.rbttn_Others.Size = new System.Drawing.Size(141, 29);
+            this.rbttn_Others.TabIndex = 9;
+            this.rbttn_Others.TabStop = true;
+            this.rbttn_Others.Text = "radioButton3";
+            this.rbttn_Others.UseVisualStyleBackColor = true;
+            // 
+            // rbttn_Female
+            // 
+            this.rbttn_Female.AutoSize = true;
+            this.rbttn_Female.Location = new System.Drawing.Point(50, 210);
+            this.rbttn_Female.Name = "rbttn_Female";
+            this.rbttn_Female.Size = new System.Drawing.Size(141, 29);
+            this.rbttn_Female.TabIndex = 8;
+            this.rbttn_Female.TabStop = true;
+            this.rbttn_Female.Text = "radioButton2";
+            this.rbttn_Female.UseVisualStyleBackColor = true;
+            // 
+            // rbttn_Male
+            // 
+            this.rbttn_Male.AutoSize = true;
+            this.rbttn_Male.Location = new System.Drawing.Point(50, 180);
+            this.rbttn_Male.Name = "rbttn_Male";
+            this.rbttn_Male.Size = new System.Drawing.Size(141, 29);
+            this.rbttn_Male.TabIndex = 7;
+            this.rbttn_Male.TabStop = true;
+            this.rbttn_Male.Text = "radioButton1";
+            this.rbttn_Male.UseVisualStyleBackColor = true;
+            // 
+            // lbl_Gender
+            // 
+            this.lbl_Gender.AutoSize = true;
+            this.lbl_Gender.Location = new System.Drawing.Point(30, 150);
+            this.lbl_Gender.Name = "lbl_Gender";
+            this.lbl_Gender.Size = new System.Drawing.Size(59, 25);
+            this.lbl_Gender.TabIndex = 6;
+            this.lbl_Gender.Text = "label5";
+            // 
+            // lbl_LastName
+            // 
+            this.lbl_LastName.AutoSize = true;
+            this.lbl_LastName.Location = new System.Drawing.Point(210, 120);
+            this.lbl_LastName.Name = "lbl_LastName";
+            this.lbl_LastName.Size = new System.Drawing.Size(59, 25);
+            this.lbl_LastName.TabIndex = 5;
+            this.lbl_LastName.Text = "label4";
+            // 
+            // lbl_FirstName
+            // 
+            this.lbl_FirstName.AutoSize = true;
+            this.lbl_FirstName.Location = new System.Drawing.Point(40, 120);
+            this.lbl_FirstName.Name = "lbl_FirstName";
+            this.lbl_FirstName.Size = new System.Drawing.Size(59, 25);
+            this.lbl_FirstName.TabIndex = 4;
+            this.lbl_FirstName.Text = "label3";
+            // 
+            // txt_LastName
+            // 
+            this.txt_LastName.Location = new System.Drawing.Point(210, 80);
+            this.txt_LastName.Name = "txt_LastName";
+            this.txt_LastName.Size = new System.Drawing.Size(150, 31);
+            this.txt_LastName.TabIndex = 3;
+            // 
+            // txt_FirstName
+            // 
+            this.txt_FirstName.Location = new System.Drawing.Point(40, 80);
+            this.txt_FirstName.Name = "txt_FirstName";
+            this.txt_FirstName.Size = new System.Drawing.Size(150, 31);
+            this.txt_FirstName.TabIndex = 2;
+            // 
+            // lbl_Name
+            // 
+            this.lbl_Name.AutoSize = true;
+            this.lbl_Name.Location = new System.Drawing.Point(30, 50);
+            this.lbl_Name.Name = "lbl_Name";
+            this.lbl_Name.Size = new System.Drawing.Size(59, 25);
+            this.lbl_Name.TabIndex = 1;
+            this.lbl_Name.Text = "label2";
+            // 
+            // lbl_PersonalDetails
+            // 
+            this.lbl_PersonalDetails.AutoSize = true;
+            this.lbl_PersonalDetails.Location = new System.Drawing.Point(10, 10);
+            this.lbl_PersonalDetails.Name = "lbl_PersonalDetails";
+            this.lbl_PersonalDetails.Size = new System.Drawing.Size(59, 25);
+            this.lbl_PersonalDetails.TabIndex = 0;
+            this.lbl_PersonalDetails.Text = "label1";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(890, 70);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(320, 160);
+            this.textBox1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(940, 390);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 34);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 25F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private Panel panel1;
+        private DateTimePicker dtpckr_Birthdate;
+        private TextBox txt_Age;
+        private Label lbl_Age;
+        private TextBox txt_Others;
+        private RadioButton rbttn_Others;
+        private RadioButton rbttn_Female;
+        private RadioButton rbttn_Male;
+        private Label lbl_Gender;
+        private Label lbl_LastName;
+        private Label lbl_FirstName;
+        private TextBox txt_LastName;
+        private TextBox txt_FirstName;
+        private Label lbl_Name;
+        private Label lbl_PersonalDetails;
+        private Label lbl_Province;
+        private Label lbl_Municipality;
+        private Label lbl_Barangay;
+        private Label lbl_Street;
+        private Label lbl_HouseNumber;
+        private TextBox txt_Province;
+        private TextBox txt_Municipality;
+        private TextBox txt_Barangay;
+        private TextBox txt_Street;
+        private TextBox txt_HouseNumber;
+        private Label lbl_Address;
+        private TextBox txt_Contact;
+        private Label lbl_Contact;
+        private TextBox txt_Email;
+        private Label lbl_Email;
+        private Label lbl_Birthdate;
+        private Label lbl_YearsOld;
+        private TextBox textBox1;
+        private Button button1;
     }
 }
--- Contact Tracing/Form1.cs
+++ Contact Tracing/Form1.cs
@@ -1,3 +1,7 @@
+using System;
+using System.IO;
+using System.Windows.Forms;
+
 namespace Contact_Tracing
 {
     public partial class Form1 : Form
@@ -5,6 +9,14 @@ namespace Contact_Tracing
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\POIs\\" + txt_FirstName.Text + " " + txt_LastName.Text + ".txt");
+            string FullName = txt_FirstName.Text + " " + txt_LastName.Text;
+            sw.WriteLine(FullName);
+            sw.Close();
         }
     }
 }
