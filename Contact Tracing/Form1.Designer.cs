256bac1c0eb0a72f4ca7a242cbcae143bf4bce3a
Lenard Rodriguez <rodriguezjanlenard.new@gmail.com>
2022-07-01 00:19:58 +0800

    created class contact

--- Contact Tracing/Form1.Designer.cs
+++ Contact Tracing/Form1.Designer.cs
@@ -29,7 +29,337 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.pnlMenu = new System.Windows.Forms.Panel();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.bttnView = new System.Windows.Forms.Button();
+            this.bttnRegister = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.pnlSurvey = new System.Windows.Forms.Panel();
+            this.tester = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.txtaMunicipal = new System.Windows.Forms.TextBox();
+            this.txtaTown = new System.Windows.Forms.TextBox();
+            this.txtaStreet = new System.Windows.Forms.TextBox();
+            this.txtaNo = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtContactNo = new System.Windows.Forms.TextBox();
+            this.cGender = new System.Windows.Forms.ComboBox();
+            this.txtAge = new System.Windows.Forms.TextBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.txtlName = new System.Windows.Forms.TextBox();
+            this.txtfName = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlQR = new System.Windows.Forms.Panel();
+            this.pnlList = new System.Windows.Forms.Panel();
+            this.pnlMenu.SuspendLayout();
+            this.pnlSurvey.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // pnlMenu
+            // 
+            this.pnlMenu.Controls.Add(this.textBox1);
+            this.pnlMenu.Controls.Add(this.bttnView);
+            this.pnlMenu.Controls.Add(this.bttnRegister);
+            this.pnlMenu.Controls.Add(this.label1);
+            this.pnlMenu.Location = new System.Drawing.Point(30, 30);
+            this.pnlMenu.Name = "pnlMenu";
+            this.pnlMenu.Size = new System.Drawing.Size(1200, 600);
+            this.pnlMenu.TabIndex = 0;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(60, 100);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(1030, 190);
+            this.textBox1.TabIndex = 3;
+            // 
+            // bttnView
+            // 
+            this.bttnView.Location = new System.Drawing.Point(70, 480);
+            this.bttnView.Name = "bttnView";
+            this.bttnView.Size = new System.Drawing.Size(230, 34);
+            this.bttnView.TabIndex = 2;
+            this.bttnView.Text = "View Contact List";
+            this.bttnView.UseVisualStyleBackColor = true;
+            // 
+            // bttnRegister
+            // 
+            this.bttnRegister.Location = new System.Drawing.Point(70, 430);
+            this.bttnRegister.Name = "bttnRegister";
+            this.bttnRegister.Size = new System.Drawing.Size(230, 34);
+            this.bttnRegister.TabIndex = 1;
+            this.bttnRegister.Text = "Register Contact";
+            this.bttnRegister.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(139, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Health Assistant";
+            // 
+            // pnlSurvey
+            // 
+            this.pnlSurvey.AllowDrop = true;
+            this.pnlSurvey.Controls.Add(this.tester);
+            this.pnlSurvey.Controls.Add(this.label15);
+            this.pnlSurvey.Controls.Add(this.label14);
+            this.pnlSurvey.Controls.Add(this.label13);
+            this.pnlSurvey.Controls.Add(this.label12);
+            this.pnlSurvey.Controls.Add(this.label11);
+            this.pnlSurvey.Controls.Add(this.label10);
+            this.pnlSurvey.Controls.Add(this.txtaMunicipal);
+            this.pnlSurvey.Controls.Add(this.txtaTown);
+            this.pnlSurvey.Controls.Add(this.txtaStreet);
+            this.pnlSurvey.Controls.Add(this.txtaNo);
+            this.pnlSurvey.Controls.Add(this.txtEmail);
+            this.pnlSurvey.Controls.Add(this.txtContactNo);
+            this.pnlSurvey.Controls.Add(this.cGender);
+            this.pnlSurvey.Controls.Add(this.txtAge);
+            this.pnlSurvey.Controls.Add(this.label9);
+            this.pnlSurvey.Controls.Add(this.txtlName);
+            this.pnlSurvey.Controls.Add(this.txtfName);
+            this.pnlSurvey.Controls.Add(this.label8);
+            this.pnlSurvey.Controls.Add(this.label7);
+            this.pnlSurvey.Controls.Add(this.label6);
+            this.pnlSurvey.Controls.Add(this.label4);
+            this.pnlSurvey.Controls.Add(this.label3);
+            this.pnlSurvey.Controls.Add(this.label2);
+            this.pnlSurvey.Location = new System.Drawing.Point(30, 30);
+            this.pnlSurvey.Name = "pnlSurvey";
+            this.pnlSurvey.Size = new System.Drawing.Size(1200, 600);
+            this.pnlSurvey.TabIndex = 0;
+            // 
+            // tester
+            // 
+            this.tester.Location = new System.Drawing.Point(860, 60);
+            this.tester.Name = "tester";
+            this.tester.Size = new System.Drawing.Size(270, 250);
+            this.tester.TabIndex = 25;
+            this.tester.Text = "test";
+            this.tester.UseVisualStyleBackColor = true;
+            this.tester.Click += new System.EventHandler(this.tester_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Location = new System.Drawing.Point(720, 450);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(107, 25);
+            this.label15.TabIndex = 24;
+            this.label15.Text = "Municipality";
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Location = new System.Drawing.Point(560, 450);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(53, 25);
+            this.label14.TabIndex = 23;
+            this.label14.Text = "Town";
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Location = new System.Drawing.Point(410, 450);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(57, 25);
+            this.label13.TabIndex = 22;
+            this.label13.Text = "Street";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(250, 450);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(96, 25);
+            this.label12.TabIndex = 21;
+            this.label12.Text = "House No.";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(460, 180);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(95, 25);
+            this.label11.TabIndex = 20;
+            this.label11.Text = "Last Name";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Location = new System.Drawing.Point(240, 180);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(97, 25);
+            this.label10.TabIndex = 19;
+            this.label10.Text = "First Name";
+            // 
+            // txtaMunicipal
+            // 
+            this.txtaMunicipal.Location = new System.Drawing.Point(720, 410);
+            this.txtaMunicipal.Name = "txtaMunicipal";
+            this.txtaMunicipal.Size = new System.Drawing.Size(150, 31);
+            this.txtaMunicipal.TabIndex = 18;
+            // 
+            // txtaTown
+            // 
+            this.txtaTown.Location = new System.Drawing.Point(560, 410);
+            this.txtaTown.Name = "txtaTown";
+            this.txtaTown.Size = new System.Drawing.Size(150, 31);
+            this.txtaTown.TabIndex = 17;
+            // 
+            // txtaStreet
+            // 
+            this.txtaStreet.Location = new System.Drawing.Point(400, 410);
+            this.txtaStreet.Name = "txtaStreet";
+            this.txtaStreet.Size = new System.Drawing.Size(150, 31);
+            this.txtaStreet.TabIndex = 16;
+            // 
+            // txtaNo
+            // 
+            this.txtaNo.Location = new System.Drawing.Point(240, 410);
+            this.txtaNo.Name = "txtaNo";
+            this.txtaNo.Size = new System.Drawing.Size(150, 31);
+            this.txtaNo.TabIndex = 15;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(240, 360);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(230, 31);
+            this.txtEmail.TabIndex = 14;
+            // 
+            // txtContactNo
+            // 
+            this.txtContactNo.Location = new System.Drawing.Point(240, 310);
+            this.txtContactNo.Name = "txtContactNo";
+            this.txtContactNo.Size = new System.Drawing.Size(230, 31);
+            this.txtContactNo.TabIndex = 13;
+            // 
+            // cGender
+            // 
+            this.cGender.FormattingEnabled = true;
+            this.cGender.Items.AddRange(new object[] {
+            "Male",
+            "Female",
+            "Bisexual",
+            "Transgender",
+            "Queer"});
+            this.cGender.Location = new System.Drawing.Point(240, 260);
+            this.cGender.Name = "cGender";
+            this.cGender.Size = new System.Drawing.Size(182, 33);
+            this.cGender.TabIndex = 12;
+            // 
+            // txtAge
+            // 
+            this.txtAge.Location = new System.Drawing.Point(240, 210);
+            this.txtAge.Name = "txtAge";
+            this.txtAge.Size = new System.Drawing.Size(60, 31);
+            this.txtAge.TabIndex = 10;
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(70, 420);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(77, 25);
+            this.label9.TabIndex = 9;
+            this.label9.Text = "Address";
+            // 
+            // txtlName
+            // 
+            this.txtlName.Location = new System.Drawing.Point(460, 140);
+            this.txtlName.Name = "txtlName";
+            this.txtlName.Size = new System.Drawing.Size(210, 31);
+            this.txtlName.TabIndex = 8;
+            // 
+            // txtfName
+            // 
+            this.txtfName.Location = new System.Drawing.Point(240, 140);
+            this.txtfName.Name = "txtfName";
+            this.txtfName.Size = new System.Drawing.Size(210, 31);
+            this.txtfName.TabIndex = 7;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(70, 370);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 25);
+            this.label8.TabIndex = 6;
+            this.label8.Text = "Email";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(70, 320);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(143, 25);
+            this.label7.TabIndex = 5;
+            this.label7.Text = "Contact Number";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(70, 270);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(69, 25);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Gender";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(70, 220);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(44, 25);
+            this.label4.TabIndex = 2;
+            this.label4.Text = "Age";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(70, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(59, 25);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Name";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(60, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(139, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Health Assistant";
+            // 
+            // pnlQR
+            // 
+            this.pnlQR.Location = new System.Drawing.Point(30, 30);
+            this.pnlQR.Name = "pnlQR";
+            this.pnlQR.Size = new System.Drawing.Size(1200, 600);
+            this.pnlQR.TabIndex = 0;
+            // 
+            // pnlList
+            // 
+            this.pnlList.Location = new System.Drawing.Point(0, 0);
+            this.pnlList.Name = "pnlList";
+            this.pnlList.Size = new System.Drawing.Size(1200, 600);
+            this.pnlList.TabIndex = 0;
             // 
             // Form1
             // 
@@ -37,13 +367,54 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ActiveBorder;
             this.ClientSize = new System.Drawing.Size(1258, 664);
+            this.Controls.Add(this.pnlSurvey);
+            this.Controls.Add(this.pnlMenu);
+            this.Controls.Add(this.pnlList);
+            this.Controls.Add(this.pnlQR);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "Form1";
             this.Text = "Health Assistant";
+            this.pnlMenu.ResumeLayout(false);
+            this.pnlMenu.PerformLayout();
+            this.pnlSurvey.ResumeLayout(false);
+            this.pnlSurvey.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private Panel pnlMenu;
+        private TextBox textBox1;
+        private Button bttnView;
+        private Button bttnRegister;
+        private Label label1;
+        private Panel pnlSurvey;
+        private TextBox txtAge;
+        private Label label9;
+        private TextBox txtlName;
+        private TextBox txtfName;
+        private Label label8;
+        private Label label7;
+        private Label label6;
+        private Label label4;
+        private Label label3;
+        private Label label2;
+        private Panel pnlQR;
+        private Panel pnlList;
+        private ComboBox cGender;
+        private Label label15;
+        private Label label14;
+        private Label label13;
+        private Label label12;
+        private Label label11;
+        private Label label10;
+        private TextBox txtaMunicipal;
+        private TextBox txtaTown;
+        private TextBox txtaStreet;
+        private TextBox txtaNo;
+        private TextBox txtEmail;
+        private TextBox txtContactNo;
+        private Button tester;
     }
 }
--- Contact Tracing/Form1.cs
+++ Contact Tracing/Form1.cs
@@ -11,6 +11,33 @@ namespace Contact_Tracing
         {
             InitializeComponent();
         }
- 
+
+        private void tester_Click(object sender, EventArgs e)
+        {
+            Contact contact = new Contact();
+            contact.GetName(txtfName.Text, txtlName.Text);
+            contact.GetGender(this.cGender.GetItemText(this.cGender.SelectedItem));
+        }
+    }
+
+    public class Contact
+    {
+        private string name = string.Empty;
+        private string age = string.Empty;  
+        private string birthday = string.Empty;
+        private string gender = string.Empty;
+        private string contact_no = string.Empty;
+        private string email = string.Empty;
+        private string address = string.Empty;
+        
+        public void GetName(string fName, string lName)
+        {
+            name = fName + " " + lName;
+        }
+        
+        public void GetGender(string Gender)
+        {
+            gender = Gender;
+        }
     }
 }
