d6236d4f99ddec1177f56a7d515d570f77594fbb
Lenard Rodriguez <rodriguezjanlenard.new@gmail.com>
2022-07-01 16:22:30 +0800

    property

--- Contact Tracing/Form1.cs
+++ Contact Tracing/Form1.cs
@@ -12,6 +12,9 @@ namespace Contact_Tracing
             InitializeComponent();
         }
 
+        StreamWriter sw = new StreamWriter(Application.StartupPath + "\\Contact\\" + "ContactTracing.txt", append: true);
+
+
         private void bttnSave_Click(object sender, EventArgs e)
         {
             if (txtfName.Text == string.Empty || txtlName.Text == string.Empty ||
@@ -23,57 +26,50 @@ namespace Contact_Tracing
             }
             else
             {
-                string Name = txtfName.Text + " " + txtlName.Text;
-                string Age = txtAge.Text;
-                string Birth = dtpBirth.Text;
-                string Gender = lbGender.Text;
-                string ContactNo = txtContactNo.Text;
-                string Email = txtEmail.Text;
-                string Address = txtaNo.Text + ", " + txtaSt.Text + ", " + txtaTown.Text + ", " + txtaMunicipal.Text;
-                string Type = lbType.Text;
+                Contact mycontact = new();
+                mycontact.Name = txtfName.Text + " " + txtlName.Text;
+                mycontact.Age = txtAge.Text;
+                mycontact.Birth = dtpBirth.Text;
+                mycontact.Gender = lbGender.Text;
+                mycontact.ContactNo = txtContactNo.Text;
+                mycontact.Email = txtEmail.Text;
+                mycontact.Address = txtaNo.Text + ", " + txtaSt.Text + ", " + txtaTown.Text + ", " + txtaMunicipal.Text;
+                mycontact.Type = lbType.Text;
 
-                Contact mycontact = new(Name, Age, Birth, Gender, ContactNo, Email, Address, Type);
+
                 mycontact.ShowDetails();
-
-                StreamWriter sw = new StreamWriter();
-
             }       
         }
     }
 
     public class Contact
     {
-        private string name = string.Empty;
-        private string age = string.Empty;  
-        private string birth = string.Empty;
-        private string gender = string.Empty;
-        private string contactno = string.Empty;
-        private string email = string.Empty;
-        private string address = string.Empty;
-        private string type = string.Empty;
+        public string? Name { get; set; }
+        public string? Age { get; set; }
+        public string? Birth { get; set; }
+        public string? Gender { get; set; }
+        public string? ContactNo { get; set; }
+        public string? Email { get; set; }
+        public string? Address { get; set; }
+        public string? Type { get; set; }
 
-        public Contact(string name, string age, string birth, string gender, string contact_no, string email, string address, string type)
+        public void WriteToText()
         {
-            this.name = name;
-            this.age = age;
-            this.birth = birth;
-            this.gender = gender;
-            this.contactno = contact_no;
-            this.email = email;
-            this.address = address;
-            this.type = type;
+            StreamWriter sw = new StreamWriter(Application.StartupPath + "\\Contact\\" + "ContactTracing.txt", append: true);
+            sw.WriteLine(Name, Age, Birth, Gender, ContactNo, Email, Address, Type);
+            sw.Close();
         }
 
         public void ShowDetails()
         {
-            MessageBox.Show(name + Environment.NewLine +
-                            age + Environment.NewLine +
-                            birth + Environment.NewLine +
-                            gender + Environment.NewLine +
-                            contactno + Environment.NewLine +
-                            email + Environment.NewLine +
-                            address + Environment.NewLine +
-                            type + Environment.NewLine);
+            MessageBox.Show(Name + Environment.NewLine +
+                            Age + Environment.NewLine +
+                            Birth + Environment.NewLine +
+                            Gender + Environment.NewLine +
+                            ContactNo + Environment.NewLine +
+                            Email + Environment.NewLine +
+                            Address + Environment.NewLine +
+                            Type + Environment.NewLine);
         }
 
         
